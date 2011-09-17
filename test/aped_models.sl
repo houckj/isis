houckj/isis

commit 3e2c8cf6935fff038206e3e6f5f4990a5a9367e6

Don't generate NaNs when ion fraction rescaling involves 0/0.

src/db-cie.c
@@ -996,7 +996,8 @@ static int get_ioniz_factor (float f_ioniz[ISIS_MAX_PROTON_NUMBER+1][ISIS_MAX_PR
 {
    int dim = ISIS_MAX_PROTON_NUMBER+1;
    int size = dim*dim;
-   int Z;
+   int Z, num_rescale_failures = 0;
+   int num_failures[ISIS_MAX_PROTON_NUMBER+1];
 
    if ((NULL == t_old)
        || (par == NULL)
@@ -1008,6 +1009,7 @@ static int get_ioniz_factor (float f_ioniz[ISIS_MAX_PROTON_NUMBER+1][ISIS_MAX_PR
    for (Z = 1; Z <= ISIS_MAX_PROTON_NUMBER; Z++)
      {
         int q;
+        num_failures[Z] = 0;
         if ((-1 == t_old->offset[Z])
             || (t_new != NULL && -1 == t_new->offset[Z]))
           {
@@ -1018,7 +1020,7 @@ static int get_ioniz_factor (float f_ioniz[ISIS_MAX_PROTON_NUMBER+1][ISIS_MAX_PR
           {
              for (q = 0; q <= Z; q++)
                {
-                  float f_old, f_new;
+                  float f_old, f_new, ff;
 
                   if (get_ion_fraction (&f_old, par, Z, q, t_old) != 0)
                     f_old = 1.0;
@@ -1028,13 +1030,46 @@ static int get_ioniz_factor (float f_ioniz[ISIS_MAX_PROTON_NUMBER+1][ISIS_MAX_PR
                        if (get_ion_fraction (&f_new, par, Z, q, t_new) != 0)
                          f_new = 1.0;
                     }
-                  else
+                  else f_new = ionpop_new[Z*dim + q];
+
+                  if ((f_old == 0.0) && (f_new > 0.0))
                     {
-                       f_new = ionpop_new[Z*dim + q];
+                       num_rescale_failures++;
+                       num_failures[Z] += 1;
                     }
 
-                  f_ioniz[Z][q] = f_new / f_old;
+                  ff = f_new / f_old;
+
+                  /* avoid NaN if f_new=f_old=0
+                   * For example, H is usually present with abund > 0
+                   * but with neutral fraction = 0.  To avoid 0/0 for
+                   * neutral H when rescaling the ionization fractions,
+                   * we have to force the correction factor =1 at this point.
+                   */
+                  f_ioniz[Z][q] = isfinite(ff) ? ff : 1.0;
                }
+          }
+     }
+
+   if (num_rescale_failures)
+     {
+        isis_vmesg (WARN, I_WARNING, __FILE__, __LINE__,
+                    "Ion fraction rescaling problem: at T=%g K, n=%g cm^-3, %d ion fractions could not be rescaled because the `old' ion fraction is zero.",
+                    par[0], par[1], num_rescale_failures);
+        if (Isis_Verbose >= WARN)
+          {
+             fprintf (stderr, "Num failures: ");
+             for (Z = 1; Z <= ISIS_MAX_PROTON_NUMBER; Z++)
+               {
+                  char el_name[3];
+                  if (num_failures[Z] == 0)
+                    continue;
+                  if (0 == _DB_get_element_name (el_name, Z))
+                    fprintf (stderr, " %s:%d", el_name, num_failures[Z]);
+                  else
+                    fprintf (stderr, " <Z=%d>:%d", Z, num_failures[Z]);
+               }
+             fprintf (stderr, "\n");
           }
      }
 
@@ -3267,7 +3302,14 @@ static int interpolate_cont_emis (EM_t *em, EM_cont_select_t *s,  /*{{{*/
 
    for (i = 0; i < n; i++)               /* loop over interpolation points */
      {
+        float abund_factor;
+
         iz = iz0;
+
+        abund_factor = f_abund[iz] * s->rel_abun[iz];
+        if (abund_factor <= 0)
+          continue;
+
         do
           {
              if (s->q < 0 && alt_ioniz)
@@ -3285,14 +3327,9 @@ static int interpolate_cont_emis (EM_t *em, EM_cont_select_t *s,  /*{{{*/
                        found_something = 1;
                        found_Z[iz] = 1;
 
-                       weight = coef[i];
-
-                       if (iz > 0)
-                         {
-                            weight *= f_abund[iz] * s->rel_abun[iz];
-                            if (iq >= 0)
-                              weight *= f_ioniz[iz][iq];
-                         }
+                       weight = coef[i] * abund_factor;
+                       if (iq >= 0)
+                         weight *= f_ioniz[iz][iq];
 
                        if (-1 == add_cont_contrib (r, t, weight))
                          return -1;

commit 85917a6122043d3da94a5706be21e2e44cf4d8bb

Fix bug in continuum abundance scaling.
   Commit 3e2c8cf693 broke this and the subsequent fix
   1f392d8b3 supposedly fixed it, but didn't.
   This time, I think it's actually fixed.
   I've added a regression test to help prevent future breakage.

src/db-cie.c
@@ -3227,6 +3227,7 @@ static int interpolate_cont_emis (EM_t *em, EM_cont_select_t *s,  /*{{{*/
    EM_cont_emis_t *t;
    float f_ioniz[ISIS_MAX_PROTON_NUMBER+1][ISIS_MAX_PROTON_NUMBER+1];
    float f_abund[ISIS_MAX_PROTON_NUMBER+1];
+   float abund_factor, weight;
    int found_Z[ISIS_MAX_PROTON_NUMBER+1];
    int alt_ioniz, alt_abund, missing_total_continuum;
    int iz, iz0, iz1;
@@ -3286,6 +3287,8 @@ static int interpolate_cont_emis (EM_t *em, EM_cont_select_t *s,  /*{{{*/
             f_ioniz[i][j] = 1.0;
      }
 
+   /* fprintf (stderr, "interpolate_cont_emis: s->Z = %d  s->q = %d\n", s->Z, s->q); */
+
    iz0 = iz1 = s->Z;     /* by default, execute iz, iq loop bodies */
    iq0 = iq1 = s->q;     /* once only */
 
@@ -3295,48 +3298,32 @@ static int interpolate_cont_emis (EM_t *em, EM_cont_select_t *s,  /*{{{*/
         iz0 = 1; iz1 = ISIS_MAX_PROTON_NUMBER;
      }
 
-   /* calling routine zeros r->(stuff) */
-
-   for (i = 0; i < n; i++)               /* loop over interpolation points */
+   for (i = 0; i < n; i++)
      {
-        float abund_factor;
-
-        iz = iz0;
-
-        abund_factor = f_abund[iz] * ((iz == 0) ? 1 : s->rel_abun[iz]);
-        if (abund_factor <= 0)
-          continue;
-
-        do
+        for (iz = iz0; iz <= iz1; iz++)
           {
+             abund_factor = f_abund[iz] * s->rel_abun[iz];
+             if (abund_factor <= 0)
+               continue;
+
              if (s->q < 0 && alt_ioniz)
                {
                   iq0 = 0;   iq1 = iz;
                }
 
-             iq = iq0;
-             do
+             for (iq = iq0; iq <= iq1; iq++)
                {
-                  float weight;
-
-                  if (NULL != (t = find_cont_type (table[i], iz, iq)))
-                    {
-                       found_something = 1;
-                       found_Z[iz] = 1;
-
-                       weight = coef[i] * abund_factor;
-                       if (iq >= 0)
-                         weight *= f_ioniz[iz][iq];
-
-                       if (-1 == add_cont_contrib (r, t, weight))
-                         return -1;
-                    }
-
-                  iq++;
-               } while (iq < iq1);        /* end loop over ions */
-
-             iz++;
-          } while (iz <= iz1);            /* end loop over elements */
+                  if (NULL == (t = find_cont_type (table[i], iz, iq)))
+                    continue;
+                  found_something = 1;
+                  found_Z[iz] = 1;
+                  weight = coef[i] * abund_factor;
+                  if (iq >= 0)
+                    weight *= f_ioniz[iz][iq];
+                  if (-1 == add_cont_contrib (r, t, weight))
+                    return -1;
+               }
+          }
      }
 
    for (i = 1; i <= ISIS_MAX_PROTON_NUMBER; i++)

test/aped_models.sl
@@ -350,4 +350,77 @@ check_value ("brightest line flux from O VIII Ly alpha lookup",
              [0.2772986393051501,
               0.27665615025627893]);
 
+private define xaped_hook (id)
+{
+   return struct {contrib_flag = MODEL_CONTIN, line_list};
+}
+private define check_trace_element_contin_abundance_scaling (temp, dens, elements, abund)
+{
+   variable lo, hi, n = 8192;
+   (lo,hi) = linear_grid (1,20,n);
+
+   variable true_sum = Double_Type[n],
+     pseudo_sum = Double_Type[n];
+
+   variable i, ne = length(elements);
+   _for i (0, ne-1, 1)
+     {
+        variable Z = elements[i];
+        variable p = get_contin (lo, hi, temp, dens, Z);
+        true_sum += p.true * abund[i];
+        pseudo_sum += p.pseudo * abund[i];
+     }
+
+   variable c_sum = true_sum + pseudo_sum;
+
+   variable s = default_plasma_state ();
+   s.temperature = temp;
+   s.density = dens;
+   s.elem = elements,
+   s.elem_abund = abund,
+   create_aped_fun ("xaped", s, &xaped_hook);
+   fit_fun ("xaped");
+   variable c = 1.e-14 * eval_fun (lo, hi);
+
+   if (any (c < 0 or c_sum < 0))
+     throw ApplicationError, "negative continuum values!!??";
+
+   i = where (c != 0);
+   variable max_abs_rel_diff = max(abs(1.0 - c_sum[i] / c[i]));
+
+#iffalse
+   ylog;
+   hplot (lo,hi,c);
+   ohplot (lo, hi, c_sum);
+   plot_pause;
+#endif
+
+   return max_abs_rel_diff;
+}
+
+private variable elements, abund;
+if (result_index == 1)
+{
+   variable max_Z = 28;
+   elements = [1:max_Z];
+   abund = Double_Type[max_Z];
+   abund[1-1] = 1.0;  %  H: Z=1
+   abund[2-1] = 1.0;  % He: Z=2
+   abund[6-1] = 10;   %  C: Z=6
+   abund[8-1] = 10;   %  O: Z=8
+}
+else if (result_index == 0)
+{
+   elements = [ H, He,  C,N, O,Ne,Mg,Al,Si,S,Ar,Ca,Fe,Ni];
+   abund =    [1., 1., 10,0,10, 0, 0, 0, 0,0, 0, 0, 0, 0];
+}
+
+variable max_abs_rel_diff;
+variable temp = 1.e7, dens=1.0,
+max_abs_rel_diff = check_trace_element_contin_abundance_scaling
+                                     (temp, dens, elements, abund);
+if (max_abs_rel_diff > 1.e-6)
+throw ApplicationError,
+   "max_abs_rel_diff = $max_abs_rel_diff, expected <~5e-08"$;
+
 msg ("ok\n");