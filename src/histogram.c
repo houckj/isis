houckj/isis

commit 260c0562febdf245d1a2bf1d81db0f9753f08f96

Introduce more consistent model definition for flux-corrected data

src/histogram.c
@@ -6421,7 +6421,7 @@ int Hist_set_fit_responses (Hist_t *h, unsigned int fit_data_type, /*{{{*/
     * However, if the assigned response is not used, the fit
     * will use only a single ARF/RMF pair.
     */
-   if ((is_flux_data || want_ideal_arf(response_mask))
+   if ((/* is_flux_data || */ want_ideal_arf(response_mask))
         && (0 == Arf_is_identity (rsp.arf)))
      {
         rsp.arf = identity_arf_on_arf_grid (h);
@@ -6434,6 +6434,7 @@ int Hist_set_fit_responses (Hist_t *h, unsigned int fit_data_type, /*{{{*/
         warn_single_rsp (&rsp);
      }
 
+#if 0
    if (is_flux_data
        && Rmf_includes_effective_area (rsp.rmf))
      {
@@ -6441,6 +6442,7 @@ int Hist_set_fit_responses (Hist_t *h, unsigned int fit_data_type, /*{{{*/
         isis_vmesg (FAIL, I_INFO, __FILE__, __LINE__, " ==> Use factor_rsp() to factor out the effective area.");
         return -1;
      }
+#endif
 
    return set_fit_response (h, &rsp);
 }
@@ -7596,6 +7598,10 @@ static int rebin_flux_using_weights (Hist_t *h, /*{{{*/
         return -1;
      }
 
+   /* f(h) = (D(h)-B(h)) / t \int dy R(h,y) A(y)   [photons/s/cm^2/A]
+    * The weights are defined so that the rebinned result yields
+    * F(H) [photons/s/cm^2/A] on the big bins.
+    */
    if (-1 == apply_rebin_weights (old_flux, h->flux_weights, 1,
                                   h->orig_nbins, h->rebin, h->nbins, new_flux))
      return -1;
@@ -7604,6 +7610,9 @@ static int rebin_flux_using_weights (Hist_t *h, /*{{{*/
    lo = h->bin_lo;
    hi = h->bin_hi;
 
+   /* multiply by the bin width, dy, to yield the rebinned flux
+    * F(H) in bin-integrated units, photons/s/cm^2/bin
+    */
    for (i = 0; i < n; i++)
      {
         new_flux[i] *= hi[i] - lo[i];
@@ -7700,6 +7709,7 @@ int Hist_flux_correct (Hist_t *h, double frac, double *bkg, int using_model, /*{
    if (-1 == setup_flux_correct (h, using_model, &cts, &f, &f_err))
      return -1;
 
+   /* f(h) = (D(h)-B(h)) / t \int dy R(h,y) A(y)   [photons/s/cm^2/A]  */
    if (-1 == k->compute_flux (k, kernel_params, num_kernel_params,
                               &cts, bkg, f, f_err, &wt, options))
      goto return_status;
@@ -7753,6 +7763,58 @@ int Hist_flux_correct (Hist_t *h, double frac, double *bkg, int using_model, /*{
      }
 
    return status;
+}
+
+/*}}}*/
+
+int Hist_flux_corr_model (Hist_t *h, double *bincts) /*{{{*/
+{
+   double *wt_sum = NULL, *lo, *hi;
+   int k, n_notice, *notice_list;
+
+   if (h->flux_weights == NULL)
+     {
+        if (NULL == (h->flux_weights = isis_unit_source_response (h->kernel)))
+          return -1;
+     }
+
+   if (NULL == (wt_sum = (double *) ISIS_MALLOC (h->nbins * sizeof(double))))
+     return -1;
+
+   if (-1 == Hist_apply_rebin_and_notice_list (wt_sum, h->flux_weights, h))
+     {
+        ISIS_FREE(wt_sum);
+        return -1;
+     }
+
+   lo = h->bin_lo;
+   hi = h->bin_hi;
+   n_notice = h->n_notice;
+   notice_list = h->notice_list;
+
+   /* [bincts] = counts
+    * [wt_sum] = sec Angstrom (cm^2 counts/photon)
+    * [bincts/wt_sum] = photons/sec/cm^2/A
+    * so multiply by bin width to get bin-integrated photon flux,
+    *   photons/sec/cm^2/bin
+    *
+    * We're operating on the data in place for efficiency,
+    * so even though the array name is no longer mnemonic, the
+    * final result has flux units.
+    */
+
+   for (k = 0; k < n_notice; k++)
+     {
+        int i = notice_list[k];
+        if (wt_sum[i] != 0.0)
+          {
+             bincts[k] *= (hi[i] - lo[i]) / wt_sum[i];
+          }
+     }
+
+   ISIS_FREE(wt_sum);
+
+   return 0;
 }
 
 /*}}}*/

doc/manual.tex
@@ -4346,9 +4346,9 @@ model values.
 
 The convolved model flux is computed using the expression
 \begin{equation}
-F(h) = \int R(h,E)S(E)dE
+  F(h) = \frac{\int dE R(h,E) A(E) s(E)}{\int dE R(h,E) A(E)}.
 \end{equation}
-unless \verb|flux_corr_model_counts| was called for this data set.
+%unless \verb|flux_corr_model_counts| was called for this data set.
 
 \end{isisfunction}
 
@@ -5452,9 +5452,9 @@ This function plots the model for the specified data set
 broadened using the assigned RMF.  The convolved model
 flux is computed using the expression
 \begin{equation}
-F(h) = \int R(h,E)S(E)dE
+  F(h) = \frac{\int dE R(h,E) A(E) s(E)}{\int dE R(h,E) A(E)}.
 \end{equation}
-unless \verb|flux_corr_model_counts| was called for this data set.
+%unless \verb|flux_corr_model_counts| was called for this data set.
 
 See \verb|get_convolved_model_flux| and
 \verb|plot_model_counts| for details.
@@ -11660,36 +11660,38 @@ compared with the flux-corrected histogram (see
 \verb|flux_corr|) using the current fit-statistic. By
 definition, the model for the flux-corrected histogram is
 \begin{equation}
-  F(h) = \int dE R(h,E) s(E),
+  F(h) = \frac{\int dE R(h,E) A(E) s(E)}{\int dE R(h,E) A(E)},
 \end{equation}
 where $E$ is the incident photon energy, $s(E)$ is the model
 spectrum, $R(h,E)$ is the redistribution function or RMF and
-$h$ is the detector channel. Note that this definition is
+$h$ is the detector channel. This form ensures that the predicted
+counts are flux-corrected in the same manner as the observed,
+background-subtracted counts.  Note that this definition is
 independent of the assigned fit-kernel.  When a fit-kernel
 supports flux-correction, that operation should be consistent
 with this definition for $F(h)$.
 
-When the assigned redistribution function includes the effective
-area, an error will be generated.  Use \verb|factor_rsp| to
-factor such a response matrix, $R(h,E)$ into an effective area
-function, $a(E)$ and a normalized redistribution function,
-$r(h,E)$ satisfying $R(h,E) = r(h,E)a(E)$. If an effective area
-function, $A(E)$ has also been assigned, it may be modified to
-include the $a(E)$ factor so that the product, $R(h,E)A(E)$ is
-preserved. For example:
-\begin{verbatim}
-    A = load_arf ("arf.fits");    % arf.fits provides A(E)
-    R = load_rmf ("rsp.fits");    % RMF includes additional area factor `a'
-    a = factor_rsp (R);           % factor R(h,E) -> r(h,E)*a(E)
+% When the assigned redistribution function includes the effective
+% area, an error will be generated.  Use \verb|factor_rsp| to
+% factor such a response matrix, $R(h,E)$ into an effective area
+% function, $a(E)$ and a normalized redistribution function,
+% $r(h,E)$ satisfying $R(h,E) = r(h,E)a(E)$. If an effective area
+% function, $A(E)$ has also been assigned, it may be modified to
+% include the $a(E)$ factor so that the product, $R(h,E)A(E)$ is
+% preserved. For example:
+% \begin{verbatim}
+%     A = load_arf ("arf.fits");    % arf.fits provides A(E)
+%     R = load_rmf ("rsp.fits");    % RMF includes additional area factor `a'
+%     a = factor_rsp (R);           % factor R(h,E) -> r(h,E)*a(E)
 
- % To preserve the product, A*R = (A*a)*r = A'*r
- % replace A with A' = A*a:
+%  % To preserve the product, A*R = (A*a)*r = A'*r
+%  % replace A with A' = A*a:
 
-    A_struct = get_arf(A);
-    a_struct = get_arf(a);
-    A_struct.value *= a_struct.value;
-    put_arf (A, A_struct);
-\end{verbatim}
+%     A_struct = get_arf(A);
+%     a_struct = get_arf(a);
+%     A_struct.value *= a_struct.value;
+%     put_arf (A, A_struct);
+% \end{verbatim}
 \end{isisfunction}
 
 \begin{isisfunction}
@@ -12013,33 +12015,33 @@ Use this function to load the convolved model flux for data set
 
 The value returned by \verb|get_convolved_model_flux| is
 \begin{equation}
-    F(h) = \int dE R(h,E) S(E)
+  F(h) = \frac{\int dE R(h,E) A(E) s(E)}{\int dE R(h,E) A(E)},
 \end{equation}
 where the integral extends over the full energy range, ensuring
 that the instrumental redistribution is fully accounted for.
 
-Keep in mind that, when the RMF is not unit-normalized, the
-value computed by \verb|eval_flux| may not be directly
-comparable with the integral of $S(E)$ over the bin-width. In
-such cases, one may wish to consider using \verb|factor_rsp| to
-factor the RMF into a unit-normalized redistribution function
-and a wavelength-dependent normalization factor.  This
-normalization factor might then be shifted into the ARF.
+% Keep in mind that, when the RMF is not unit-normalized, the
+% value computed by \verb|eval_flux| may not be directly
+% comparable with the integral of $S(E)$ over the bin-width. In
+% such cases, one may wish to consider using \verb|factor_rsp| to
+% factor the RMF into a unit-normalized redistribution function
+% and a wavelength-dependent normalization factor.  This
+% normalization factor might then be shifted into the ARF.
 
-For example:
-\begin{verbatim}
-        rmf = load_rmf ("un_normalized_rmf.fits");
-        arf = load_arf ("arf.fits");
+% For example:
+% \begin{verbatim}
+%         rmf = load_rmf ("un_normalized_rmf.fits");
+%         arf = load_arf ("arf.fits");
 
-        % factor out a normalized RMF:
-        new_arf_index = factor_rsp (rmf);
+%         % factor out a normalized RMF:
+%         new_arf_index = factor_rsp (rmf);
 
-        % shift the norm factor into the ARF:
-        factor = get_arf (new_arf_index);
-        arf_struct = get_arf (arf);
-        arf_struct.value *= factor.value;
-        put_arf (arf, arf_struct);
-\end{verbatim}
+%         % shift the norm factor into the ARF:
+%         factor = get_arf (new_arf_index);
+%         arf_struct = get_arf (arf);
+%         arf_struct.value *= factor.value;
+%         put_arf (arf, arf_struct);
+% \end{verbatim}
 
 \end{isisfunction}
 

src/fit-cmds.c
@@ -2129,6 +2129,7 @@ static int add_instrumental_background (double *cts, Hist_t *h) /*{{{*/
           return -1;
      }
 
+   /* FIXME - calling this hook here is ugly. */
    if (NULL != Hist_post_model_hook (h))
      {
         int status = Hist_run_post_model_hook (h, cts, bgd);
@@ -2223,6 +2224,12 @@ static int compute_hist_model (Hist_t *h, double *bincts) /*{{{*/
 
    if (-1 == Hist_apply_rebin_and_notice_list (bincts, cts, h))
      goto finish;
+
+   if (is_flux(Fit_Data_Type))
+     {
+        if (-1 == Hist_flux_corr_model (h, bincts))
+          goto finish;
+     }
 
    if (Fit_Store_Model)
      {

src/histogram.h
@@ -224,6 +224,7 @@ extern int Hist_set_color (Hist_t *h, int color);
 extern int Hist_unset_color (Hist_t *h);
 extern int Hist_set_object_name (Hist_t *h, char *object);
 extern int Hist_get_flux_corr_weights (Hist_t *h, double **weights, int *num_weights);
+extern int Hist_flux_corr_model (Hist_t *h, double *bincts);
 
 /* misc.. */
 extern int Hist_is_fake (Hist_t *h);

src/isis.h
@@ -36,7 +36,7 @@ extern "C" {
 #include <slang.h>
 
 #define ISIS_VERSION          10602
-#define ISIS_VERSION_STRING  "1.6.2-8"
+#define ISIS_VERSION_STRING  "1.6.2-9"
 #define ISIS_VERSION_PREFIX   1.6.2
 
 #define ISIS_API_VERSION 6

commit b27f61b7ebb3338102630e936a2f6649885e8eb8

Fix indexing bug introduced by 260c0562feb

src/histogram.c
@@ -7811,7 +7811,7 @@ int Hist_flux_correct (Hist_t *h, double frac, double *bkg, int using_model, /*{
 int Hist_flux_corr_model (Hist_t *h, double *bincts) /*{{{*/
 {
    double *wt_sum = NULL, *lo, *hi;
-   int k, n_notice, *notice_list;
+   int i, n_notice, *notice_list;
 
    if (h->flux_weights == NULL)
      {
@@ -7845,12 +7845,12 @@ int Hist_flux_corr_model (Hist_t *h, double *bincts) /*{{{*/
     * final result has flux units.
     */
 
-   for (k = 0; k < n_notice; k++)
+   for (i = 0; i < n_notice; i++)
      {
-        int i = notice_list[k];
+        int k = notice_list[i];
         if (wt_sum[i] != 0.0)
           {
-             bincts[k] *= (hi[i] - lo[i]) / wt_sum[i];
+             bincts[i] *= (hi[k] - lo[k]) / wt_sum[i];
           }
      }
 